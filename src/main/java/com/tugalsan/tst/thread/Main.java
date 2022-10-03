f9552fec63e488e65832a7e2cbbc221cc5f602f3
tugalsan karabacak <tugalsan@gmail.com>
2022-10-03 08:23:17 +0300

    jdk.incubator.concurrent

--- src/main/java/com/tugalsan/tst/thread/Main.java
+++ src/main/java/com/tugalsan/tst/thread/Main.java
@@ -1,12 +1,28 @@
 package com.tugalsan.tst.thread;
 
 import com.tugalsan.api.log.server.*;
+import com.tugalsan.api.thread.server.TS_ThreadFetchAll;
+import com.tugalsan.api.thread.server.TS_ThreadWait;
+import java.time.Instant;
 
 public class Main {
 
     private static TS_Log d = TS_Log.of(Main.class);
 
-    public static void main(String[] args) {
-
+    public static void main(String... s) {
+        TS_ThreadFetchAll<String> f = TS_ThreadFetchAll.of(Instant.now().plusSeconds(5),
+                () -> {
+                    TS_ThreadWait.seconds(null, 1);
+                    return "1";
+                },
+                () -> {
+                    TS_ThreadWait.seconds(null, 2);
+                    return "2";
+                },
+                () -> {
+                    TS_ThreadWait.seconds(null, 3);
+                    return "3";
+                }
+        );
     }
 }
