1f789000dc95708334a76f8a4fc3a5c1d2b86b8f
tugalsan karabacak <tugalsan@gmail.com>
2022-10-04 04:34:54 +0300

    updating module req

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.tugalsan.tst.thread {
     requires gwt.user;
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.compiler;
+    requires com.tugalsan.api.random;
+    requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.executable;
     requires com.tugalsan.api.validator;
     requires com.tugalsan.api.pack;
