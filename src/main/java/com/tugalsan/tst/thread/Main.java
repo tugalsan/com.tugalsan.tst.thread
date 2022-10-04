31c39e75cdb7456f566168db657da228e127766f
tugalsan karabacak <tugalsan@gmail.com>
2022-10-04 08:59:38 +0300

    minor

--- src/main/java/com/tugalsan/tst/thread/Main.java
+++ src/main/java/com/tugalsan/tst/thread/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
     private static final TS_Log d = TS_Log.of(Main.class);
 
+    //cd C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.thread
     //java --enable-preview --add-modules jdk.incubator.concurrent -jar target/com.tugalsan.tst.thread-1.0-SNAPSHOT-jar-with-dependencies.jar
     public static void main(String... s) {
         List<Callable<String>> callables = List.of(
