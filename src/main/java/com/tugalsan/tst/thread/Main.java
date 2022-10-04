5e2a5c7e9a7750b9ff24408f1743ccdaa9a95190
tugalsan karabacak <tugalsan@gmail.com>
2022-10-04 08:56:13 +0300

    minor

--- src/main/java/com/tugalsan/tst/thread/Main.java
+++ src/main/java/com/tugalsan/tst/thread/Main.java
@@ -33,50 +33,20 @@ public class Main {
                     return "3";
                 }
         );
-        TS_ThreadFetchAll<String> fetchAll = TS_ThreadFetchAll.of(
-                /*Instant.now().plusSeconds(5)*/null,
-                () -> {
-                    d.cr("fetchAll.fetcing...", "1");
-                    TS_ThreadWait.seconds(null, 1);
-                    d.cr("fetchAll.completed", "1");
-                    return "1";
-                },
-                () -> {
-                    d.cr("fetchAll.fetcing...", "2");
-                    TS_ThreadWait.seconds(null, 2);
-                    d.cr("fetchAll.completed", "2");
-                    return "2";
-                },
-                () -> {
-                    d.cr("fetchAll.fetcing...", "3");
-                    TS_ThreadWait.seconds(null, 3);
-                    d.cr("fetchAll.completed", "3");
-                    return "3";
-                }
-        );
-        fetchAll.resultLst().forEach(result -> d.cr("result", result));
-        TS_ThreadFetchFirst<String> fetchFirst = TS_ThreadFetchFirst.of(
-                /*Instant.now().plusSeconds(5)*/null,
-                () -> {
-                    d.cr("fetchFirst.fetcing...", "1");
-                    TS_ThreadWait.seconds(null, 1);
-                    d.cr("fetchFirst.completed", "1");
-                    return "1";
-                },
-                () -> {
-                    d.cr("fetchFirst.fetcing...", "2");
-                    TS_ThreadWait.seconds(null, 2);
-                    d.cr("fetchFirst.completed", "2");
-                    return "2";
-                },
-                () -> {
-                    d.cr("fetchFirst.fetcing...", "3");
-                    TS_ThreadWait.seconds(null, 3);
-                    d.cr("fetchFirst.completed", "3");
-                    return "3";
-                }
-        );
-        d.cr("fetchFirst.result()", fetchFirst.result());
+        if (true) {
+            TS_ThreadFetchAll.of(
+                    /*Instant.now().plusSeconds(5)*/null,
+                    callables
+            ).resultLst().forEach(result -> d.cr("result", result));
+        }
+        if (false) {
+            d.cr("fetchFirst.result()",
+                    TS_ThreadFetchFirst.of(
+                            /*Instant.now().plusSeconds(5)*/null,
+                            callables
+                    ).result()
+            );
+        }
         TS_ThreadWait.seconds(null, 10);
     }
 }
